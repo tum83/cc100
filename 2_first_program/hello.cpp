ebc3e92094cf5104bcbb7e097f787ec3f8ce1393
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2021-10-11 07:58:02 -0500

    sorting c compilers out

---
+++ 2_first_program/hello.cpp
@@ -0,0 +1,11 @@
+
+// This program outputs the message "Hello, World!" to the monitor
+
+#include <ios>
+
+int main()  // C++ programs start by executing the function main
+{
+  cout << "Hello, World!\n";  // output "Hello, World!"
+  return 0;
+}
+

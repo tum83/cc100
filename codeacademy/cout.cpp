b03a58177ea534e5c3ead7b15a07a17eb910f0e4
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2021-11-25 17:01:27 -0600

    update

---
+++ codeacademy/cout.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "1\n";
+  std::cout << "2\n";
+  std::cout << "3\n";
+}
---
+++ codeacademy/print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  int a=1;
+  int b=1;
+  printf("%d\n", a+b);
+}

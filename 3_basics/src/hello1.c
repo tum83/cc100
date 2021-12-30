9ebd445092e7fd2349211626182915a1620d2cfe
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2021-12-29 22:37:16 -0600

    updates

--- 3_basics/src/hello1.c
+++ 3_basics/src/hello1.c
@@ -3,6 +3,7 @@
 /*    * What happens here: */
 /*      - A header file (~stdio.h~) is included for input/output */
 /*      - A function (~main~) without arguments (~void~) is defined */
+/*      - The function returns integer data (~int~) */
 /*      - A string (~"..."~) is printed out */
 /*      - A new-line is added at the end (~\n~) */
 /*      - If successful, the program returns the value ~0~ */

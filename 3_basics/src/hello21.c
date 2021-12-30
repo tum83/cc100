6d6e8512b4db89b617394c60292beaf7f031e4f8
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2021-12-29 21:56:19 -0600

    updates

--- 3_basics/src/hello21.c
+++
@@ -1,8 +0,0 @@
-/* [[file:../README.org::*Version 1][Version 1:1]] */
-#include <stdio.h>
-int main(void)
-{
-  printf("Hello world\n");
-  return 0;
-}
-/* Version 1:1 ends here */
--- 3_basics/src/hellotangle.c
+++
@@ -1,4 +0,0 @@
-int main() {
-puts("hello");
-return 0;
-}
